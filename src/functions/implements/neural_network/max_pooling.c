188c42b872f0346afc2dac916a31cade773fc227
Ryota.Kunisada <Ryota.Kunisada@sony.com>
2018-02-01 06:47:38 +0000

    Move input_shape, output_shape and s as max_pooling_private_t.

--- src/functions/implements/neural_network/max_pooling.c
+++ src/functions/implements/neural_network/max_pooling.c
@@ -17,20 +17,29 @@
 
 #include <math.h>
 
+typedef struct {
+  rt_list_t input_shape;
+  rt_list_t output_shape;
+  int input_n_kernel_size_diff;
+ } max_pooling_private_t;
+
 rt_function_error_t allocate_max_pooling_local_context(rt_function_t *f) {
   max_pooling_local_context_t *context = (max_pooling_local_context_t *)(f->local_context);
-  const rt_list_t inshape = clone_list(f->inputs[0]->shape);
-  const rt_list_t outshape = clone_list(f->outputs[0]->shape);
+  max_pooling_private_t *private = malloc(sizeof(max_pooling_private_t));
+  if (private == 0) {
+    return RT_FUNCTION_ERROR_MALLOC;
+  }
+  private->input_shape = clone_list(f->inputs[0]->shape);
+  private->output_shape = clone_list(f->outputs[0]->shape);
+  private->input_n_kernel_size_diff = private->input_shape.size - context->kernel.size;
   int i;
-  rt_list_t shape = allocate_list(context->kernel.size);
-  const int s = inshape.size - context->kernel.size;
   if(context->stride.size == 0) {
     context->stride = clone_list(context->kernel);
   } else {
     if(context->kernel.size != context->stride.size) {
       return RT_FUNCTION_ERROR_INVALID_SHAPE;
     }
-    if(context->kernel.size > inshape.size) {
+    if(context->kernel.size > private->input_shape.size) {
       return RT_FUNCTION_ERROR_INVALID_SHAPE;
     }
   }
@@ -40,8 +49,9 @@ rt_function_error_t allocate_max_pooling_local_context(rt_function_t *f) {
   if(context->kernel.size != context->pad.size) {
     return RT_FUNCTION_ERROR_INVALID_SHAPE;
   }
+  rt_list_t shape = allocate_list(context->kernel.size);
   for (i = 0; i < context->kernel.size; i++) {
-    shape.data[i] = inshape.data[i + s];
+    shape.data[i] = private->input_shape.data[i + private->input_n_kernel_size_diff];
   }
   for (i = 0; i < shape.size; i++) {
     shape.data[i] += 2 * context->pad.data[i];
@@ -51,42 +61,47 @@ rt_function_error_t allocate_max_pooling_local_context(rt_function_t *f) {
       shape.data[i] = ceil(shape.data[i] * 1.0 / context->stride.data[i]);
     }
   }
-  for (i = 0; i < inshape.size; i++) {
-    if (i < s) {
-      outshape.data[i] = inshape.data[i];
+  for (i = 0; i < private->input_shape.size; i++) {
+    if (i < private->input_n_kernel_size_diff) {
+      private->output_shape.data[i] = private->input_shape.data[i];
     } else {
-      outshape.data[i] = shape.data[i - s];
+      private->output_shape.data[i] = shape.data[i - private->input_n_kernel_size_diff];
     }
   }
-  free_list(inshape);
-  free_list(outshape);
   free_list(shape);
+  ((max_pooling_local_context_t *)(f->local_context))->private = (void *)private;
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
 rt_function_error_t free_max_pooling_local_context(rt_function_t *f) {
+  max_pooling_private_t *private =
+      (max_pooling_private_t *)(((max_pooling_local_context_t *)(f->local_context))
+                               ->private);
+  free_list(private->input_shape);
+  free_list(private->output_shape);
+  free(private);
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
 rt_function_error_t exec_max_pooling(rt_function_t *f) {
   max_pooling_local_context_t *context = (max_pooling_local_context_t *)(f->local_context);
+  max_pooling_private_t *private =
+      (max_pooling_private_t *)(((max_pooling_local_context_t *)(f->local_context))
+                               ->private);
   const float *x = (float *)(f->inputs[0]->data);
   float *y = (float *)(f->outputs[0]->data);
-  const rt_list_t inshape = clone_list(f->inputs[0]->shape);
-  const rt_list_t outshape = clone_list(f->outputs[0]->shape);
-  const int s = inshape.size - context->kernel.size;
   const rt_list_t input_strides = get_c_contiguous_strides(f->inputs[0]->shape);
   const rt_list_t output_strides = get_c_contiguous_strides(f->outputs[0]->shape);
   const int x_stride =
-    (s == 0) ? calc_shape_size(f->inputs[0]->shape) : input_strides.data[s - 1];
+    (private->input_n_kernel_size_diff == 0) ? calc_shape_size(f->inputs[0]->shape) : input_strides.data[private->input_n_kernel_size_diff - 1];
   const int y_stride =
-    (s == 0) ? calc_shape_size(f->outputs[0]->shape) : output_strides.data[s - 1];
+    (private->input_n_kernel_size_diff == 0) ? calc_shape_size(f->outputs[0]->shape) : output_strides.data[private->input_n_kernel_size_diff - 1];
   free_list(input_strides);
   free_list(output_strides);
-  const int hx = inshape.data[s + 0];
-  const int wx = inshape.data[s + 1];
-  const int hy = outshape.data[s + 0];
-  const int wy = outshape.data[s + 1];
+  const int hx = private->input_shape.data[private->input_n_kernel_size_diff + 0];
+  const int wx = private->input_shape.data[private->input_n_kernel_size_diff + 1];
+  const int hy = private->output_shape.data[private->input_n_kernel_size_diff + 0];
+  const int wy = private->output_shape.data[private->input_n_kernel_size_diff + 1];
   const int hkernel = context->kernel.data[0];
   const int wkernel = context->kernel.data[1];
   const int hstride = context->stride.data[0];
@@ -94,12 +109,12 @@ rt_function_error_t exec_max_pooling(rt_function_t *f) {
   const int hpad = context->pad.data[0];
   const int wpad = context->pad.data[1];
   const int n_map = calc_shape_size(f->inputs[0]->shape) / x_stride;
-  int iy, jy, ix, jx;
+  int n, iy, jy, ix, jx;
   int hstart, wstart, hend, wend;
   int k, l;
   float max_val;
   float val;
-  for (int n = 0; n < n_map; ++n) {
+  for (n = 0; n < n_map; ++n) {
     for(iy = 0; iy < hy; ++iy) {
       for(jy = 0; jy < wy; ++jy) {
         hstart = iy * hstride - hpad;
@@ -127,7 +142,5 @@ rt_function_error_t exec_max_pooling(rt_function_t *f) {
     x += x_stride;
     y += y_stride;
   }
-  free_list(inshape);
-  free_list(outshape);
   return RT_FUNCTION_ERROR_NOERROR;
 }
