749f613277d88cb92f3692abe49e217da46e7ac2
Ryota.Kunisada <Ryota.Kunisada@sony.com>
2018-02-01 08:45:11 +0000

    Add prototype declaration to softmax.c.

--- src/functions/implements/activation/softmax.c
+++ src/functions/implements/activation/softmax.c
@@ -17,6 +17,15 @@
 
 #include "../../utilities.h"
 
+static inline float max(float a, float b);
+static inline float sum(float a, float b);
+static inline float devide(float a, float b);
+static inline float identify(float a);
+static inline void map(rt_list_t list, float (*func1)(float), float (*func2)(float, float), float arg);
+static inline float reduce(rt_list_t list, float (*func)(float, float));
+static inline void softmax_impl(const int batch_size, const int specified_axis_size, const int rest_size,
+              const int batch_index, const float *const input, float *const output);
+
 rt_function_error_t allocate_softmax_local_context(rt_function_t *f) {
   return RT_FUNCTION_ERROR_NOERROR;
 }
@@ -25,9 +34,37 @@ rt_function_error_t free_softmax_local_context(rt_function_t *f) {
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
+rt_function_error_t exec_softmax(rt_function_t *f) {
+  // set up
+  softmax_local_context_t *context = (softmax_local_context_t *)(context_of(f));
+  const int axis = context->axis;
+  const int size = calc_shape_size(input_shape_of(f, 0));
+  // axis must be less than ndim of inputs[0].
+  if (input_shape_of(f, 0).size <= axis) {
+    return RT_FUNCTION_ERROR_INVALID_SHAPE;
+  }
+  const rt_variable_t *const input = input_of(f, 0);
+  const int batch_size = shape_product_of(input, 0, axis);
+  const int specified_axis_size = input_shape_value_of(f, 0, axis);
+  const int rest_size = size / batch_size / specified_axis_size;
+  if (batch_size * specified_axis_size * rest_size != size) {
+    return RT_FUNCTION_ERROR_INVALID_SHAPE;
+  }
+  // exec
+  int sample_index;
+  for (sample_index = 0; sample_index < batch_size ; ++sample_index) {
+    softmax_impl(batch_size, specified_axis_size, rest_size, sample_index,
+            input_data_of(f, 0), output_data_of(f, 0));
+  }
+  return RT_FUNCTION_ERROR_NOERROR;
+}
+
 static inline float max(float a, float b) { return a < b ? b : a; }
+
 static inline float sum(float a, float b) { return a + b; }
+
 static inline float devide(float a, float b) { return a / b; }
+
 static inline float identify(float a) { return a; }
 
 static inline void map(rt_list_t list, float (*func1)(float), float (*func2)(float, float), float arg) {
@@ -72,29 +109,4 @@ static inline void softmax_impl(const int batch_size, const int specified_axis_s
     }
   }
   free_list(array);
-}
-
-rt_function_error_t exec_softmax(rt_function_t *f) {
-  // set up
-  softmax_local_context_t *context = (softmax_local_context_t *)(context_of(f));
-  const int axis = context->axis;
-  const int size = calc_shape_size(input_shape_of(f, 0));
-  // axis must be less than ndim of inputs[0].
-  if (input_shape_of(f, 0).size <= axis) {
-    return RT_FUNCTION_ERROR_INVALID_SHAPE;
-  }
-  const rt_variable_t *const input = input_of(f, 0);
-  const int batch_size = shape_product_of(input, 0, axis);
-  const int specified_axis_size = input_shape_value_of(f, 0, axis);
-  const int rest_size = size / batch_size / specified_axis_size;
-  if (batch_size * specified_axis_size * rest_size != size) {
-    return RT_FUNCTION_ERROR_INVALID_SHAPE;
-  }
-  // exec
-  int sample_index;
-  for (sample_index = 0; sample_index < batch_size ; ++sample_index) {
-    softmax_impl(batch_size, specified_axis_size, rest_size, sample_index,
-            input_data_of(f, 0), output_data_of(f, 0));
-  }
-  return RT_FUNCTION_ERROR_NOERROR;
 }
