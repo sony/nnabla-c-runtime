111ada1cb3228c2083b412599be0e7f9cfe7dddb
Ryota.Kunisada <Ryota.Kunisada@sony.com>
2018-02-06 05:42:15 +0000

    Move calculate sizes to allocate_softmax().

--- src/functions/implements/activation/softmax.c
+++ src/functions/implements/activation/softmax.c
@@ -17,18 +17,22 @@
 
 #include "../../utilities.h"
 
-static inline float max(float a, float b);
+static inline float max(float a, float b) {
+  return a < b ? b : a;
+}
+
+typedef struct {
+  int batch_size;
+  int specified_axis_size;
+  int output_size;
+ } softmax_private_t;
 
 rt_function_error_t allocate_softmax_local_context(rt_function_t *f) {
-  return RT_FUNCTION_ERROR_NOERROR;
-}
-
-rt_function_error_t free_softmax_local_context(rt_function_t *f) {
-  return RT_FUNCTION_ERROR_NOERROR;
-}
-
-rt_function_error_t exec_softmax(rt_function_t *f) {
   softmax_local_context_t *context = (softmax_local_context_t *)(f->local_context);
+  softmax_private_t *private = malloc(sizeof(softmax_private_t));
+  if (private == 0) {
+    return RT_FUNCTION_ERROR_MALLOC;
+  }
   const int axis = context->axis;
   const int size = calc_shape_size(f->inputs[0]->shape);
   const int size_axis = axis <= 0 ? size : shape_product_of(f->inputs[0], axis, f->inputs[0]->shape.size);
@@ -37,21 +41,39 @@ rt_function_error_t exec_softmax(rt_function_t *f) {
   if (f->inputs[0]->shape.size <= axis) {
     return RT_FUNCTION_ERROR_INVALID_SHAPE;
   }
-  const int batch_size = size / size_axis;
-  const int specified_axis_size = size_axis;
-  const int output_size = size / batch_size / specified_axis_size;
-  if (batch_size * specified_axis_size * output_size != size) {
+  private->batch_size = size / size_axis;
+  private->specified_axis_size = size_axis;
+  private->output_size = size / private->batch_size / private->specified_axis_size;
+  if (private->batch_size * private->specified_axis_size * private->output_size != size) {
     return RT_FUNCTION_ERROR_INVALID_SHAPE;
   }
+  ((softmax_local_context_t *)(f->local_context))->private = (void *)private;
+  return RT_FUNCTION_ERROR_NOERROR;
+}
 
+rt_function_error_t free_softmax_local_context(rt_function_t *f) {
+  free(((softmax_local_context_t *)(f->local_context))->private);
+  return RT_FUNCTION_ERROR_NOERROR;
+}
+
+rt_function_error_t exec_softmax(rt_function_t *f) {
+  softmax_local_context_t *context = (softmax_local_context_t *)(f->local_context);
+  softmax_private_t *private =
+      (softmax_private_t *)(context->private);
   const float *const x = (float *)(f->inputs[0]->data);
   float *const y = (float *)(f->outputs[0]->data);
-  int sample_index, specified_index, output_index;
+  const int batch_size = private->batch_size;
+  const int specified_axis_size = private->specified_axis_size;
+  const int output_size = private->output_size;
+
+  int sample_index;
   for (sample_index = 0; sample_index < batch_size ; ++sample_index) {
+    int output_index;
     for (output_index = 0; output_index < output_size ; ++output_index) {
       const int j = sample_index * specified_axis_size * output_size + output_index;
       // compute maximum
       float max_input = x[j];
+      int specified_index;
       for (specified_index = 0; specified_index < specified_axis_size ; ++specified_index) {
         const int k = specified_index * output_size + j;
         max_input = max(max_input, x[k]);
@@ -73,5 +95,3 @@ rt_function_error_t exec_softmax(rt_function_t *f) {
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
-
-static inline float max(float a, float b) { return a < b ? b : a; }
