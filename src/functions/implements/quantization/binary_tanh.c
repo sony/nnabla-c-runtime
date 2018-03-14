47b77011dd945a5307deab30ed5ffeed79a8197e
Tadashi Naka <Tadashi.Naka@sony.com>
2018-03-14 17:50:29 +0900

    Change directory structure, use private structure

--- src/functions/implements/quantization/binary_tanh.c
+++ src/functions/implements/quantization/binary_tanh.c
@@ -15,7 +15,13 @@
 #include <nnablart/functions.h>
 #include "../../utilities.h"
 #include <math.h>
-#include <assert.h>
+
+typedef struct {
+  float *input;
+  int input_size;
+  float *output;
+  int output_size;
+} binary_tanh_private_context_t;
 
 // BinaryTanh
 rt_function_error_t allocate_binary_tanh_local_context(rt_function_t *f)
@@ -26,29 +32,37 @@ rt_function_error_t allocate_binary_tanh_local_context(rt_function_t *f)
   if (f->num_of_outputs != 1) {
     return RT_FUNCTION_ERROR_INVALID_NUM_OF_OUTPUTS;
   }
+  binary_tanh_private_context_t *private = malloc(sizeof(binary_tanh_private_context_t));
+  if (private == 0) {
+    return RT_FUNCTION_ERROR_MALLOC;
+  }
+  f->local_context = private;
+  private->input = f->inputs[0]->data;
+  private->input_size = calc_shape_size(f->inputs[0]->shape);
+  private->output = f->outputs[0]->data;
+  private->output_size = calc_shape_size(f->outputs[0]->shape);
+  if (private->input_size != private->output_size) {
+    return RT_FUNCTION_ERROR_INVALID_SHAPE;
+  }
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
 rt_function_error_t free_binary_tanh_local_context(rt_function_t *f)
 {
+  free(f->local_context);
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
 rt_function_error_t exec_binary_tanh(rt_function_t *f)
 {
-  float* input;
-  float* output;
-  input = f->inputs[0]->data;
-  output = f->outputs[0]->data;
-  int in_size = calc_shape_size(f->inputs[0]->shape);
-  int out_size = calc_shape_size(f->outputs[0]->shape);
-  assert(in_size == out_size);
+  binary_tanh_private_context_t *private = (binary_tanh_private_context_t *)(f->local_context);
+  
   int i;
-  for(i = 0; i < in_size; i++) {
-    float x = (input[i] + 1) / 2;
+  for(i = 0; i < private->input_size; i++) {
+    float x = (private->input[i] + 1) / 2;
     if(x < 0) { x = 0; }
     if(x > 1) { x = 1; }
-    output[i] = roundf(x) * 2 - 1;
+    private->output[i] = roundf(x) * 2 - 1;
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
