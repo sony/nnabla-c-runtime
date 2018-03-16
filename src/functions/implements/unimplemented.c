7fa41ccee160bce530ab2b722d2f7cf66e73ad0d
Tadashi Naka <Tadashi.Naka@sony.com>
2018-03-16 16:00:28 +0900

    Implementation of RDivScalar function

--- src/functions/implements/unimplemented.c
+++ src/functions/implements/unimplemented.c
@@ -459,19 +459,6 @@ rt_function_error_t exec_r_sub_scalar(rt_function_t *f) {
   return RT_FUNCTION_ERROR_UNIMPLEMENTED;
 }
 
-// RDivScalar
-rt_function_error_t allocate_r_div_scalar_local_context(rt_function_t *f) {
-  return RT_FUNCTION_ERROR_UNIMPLEMENTED;
-}
-
-rt_function_error_t free_r_div_scalar_local_context(rt_function_t *f) {
-  return RT_FUNCTION_ERROR_UNIMPLEMENTED;
-}
-
-rt_function_error_t exec_r_div_scalar(rt_function_t *f) {
-  return RT_FUNCTION_ERROR_UNIMPLEMENTED;
-}
-
 // RPowScalar
 rt_function_error_t allocate_r_pow_scalar_local_context(rt_function_t *f) {
   return RT_FUNCTION_ERROR_UNIMPLEMENTED;
