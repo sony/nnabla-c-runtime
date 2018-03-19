e6a5f740fe0a9acc1ef12666067146be7b06d328
Tadashi Naka <Tadashi.Naka@sony.com>
2018-03-19 15:45:09 +0900

    Change function calc_rdiv local to common

--- src/functions/implements/arithmetic/r_div_scalar.c
+++ src/functions/implements/arithmetic/r_div_scalar.c
@@ -15,10 +15,7 @@
 #include <nnablart/functions.h>
 #include "../../utilities.h"
 
-static float calc_rdiv(float v1, float v2) {
-    return v2 / v1;
-}
-
+// RDivScalar
 rt_function_error_t allocate_r_div_scalar_local_context(rt_function_t *f) {
   if (f->num_of_inputs != 1) {
     return RT_FUNCTION_ERROR_INVALID_NUM_OF_INPUTS;
