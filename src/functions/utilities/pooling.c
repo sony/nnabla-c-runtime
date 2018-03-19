de38276e7494ca64efc48b2487e75b5ea4524cf5
Hisae Imada <Hisae.Imada@sony.com>
2018-03-19 15:03:50 +0900

    Delete Include for <assert.h>

--- src/functions/utilities/pooling.c
+++ src/functions/utilities/pooling.c
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <assert.h>
 #include <math.h>
 #include "pooling.h"
 #include "shape.h"
