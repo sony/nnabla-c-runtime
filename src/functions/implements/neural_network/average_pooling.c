97d615950e27daa89e9e89f5ffddb841bfe53022
Liangliang.Zhou <Liangliang.Zhou@sony.com>
2018-05-23 13:29:02 +0800

    Implement xxx-pooling generic func

--- src/functions/implements/neural_network/average_pooling.c
+++ src/functions/implements/neural_network/average_pooling.c
@@ -39,5 +39,10 @@ rt_function_error_t exec_average_pooling(rt_function_t *f) {
   pooling_private_t *p =
       (pooling_private_t
            *)(((average_pooling_local_context_t *)(f->local_context))->data);
-  return exec_pooling(f, (pooling_context_t *)context, p, calc_average);
+  if (p->calc_context.x->type == NN_DATA_TYPE_FLOAT &&
+      p->calc_context.y->type == NN_DATA_TYPE_FLOAT) {
+    return exec_pooling(f, (pooling_context_t *)context, p, calc_average);
+  } else {
+    return exec_pooling_generic(f, (pooling_context_t *)context, p, calc_average_generic);
+  }
 }
--- src/functions/implements/neural_network/pooling.c
+++ src/functions/implements/neural_network/pooling.c
@@ -87,7 +87,12 @@ rt_function_error_t allocate_pooling(rt_function_t *f,
   p->calc_context.wend = 0;
   p->calc_context.wx = p->input_shape.data[p->input_n_kernel_size_diff + 1];
   p->calc_context.pool_size = 0;
-  p->calc_context.x = (float *)(f->inputs[0]->data);
+  p->calc_context.x = f->inputs[0];
+  p->calc_context.get_x = select_getter(p->calc_context.x);
+  p->calc_context.y = f->outputs[0];
+  p->calc_context.set_y = select_setter(p->calc_context.y);
+  p->calc_context.offset_x = 0;
+  p->calc_context.offset_y = 0;
   p->calc_context.including_pad = context->including_pad;
 
   return RT_FUNCTION_ERROR_NOERROR;
@@ -103,7 +108,7 @@ rt_function_error_t free_pooling(pooling_private_t *p) {
 rt_function_error_t exec_pooling(rt_function_t *f, pooling_context_t *context,
                                  pooling_private_t *p,
                                  exec_pooling_func_t exec) {
-  float *y = (float *)(f->outputs[0]->data);
+  float *y = (float *)(p->calc_context.y->data);
   const int hx = p->input_shape.data[p->input_n_kernel_size_diff + 0];
   const int wx = p->calc_context.wx;
   const int hy = p->output_shape.data[p->input_n_kernel_size_diff + 0];
@@ -132,23 +137,81 @@ rt_function_error_t exec_pooling(rt_function_t *f, pooling_context_t *context,
         p->calc_context.wend = fminf(wend, wx);
         int k = iy * wy + jy;
         float val = exec(p->calc_context);
-        y[k] = val;
+        *(y + k + p->calc_context.offset_y) = val;
       }
     }
-    p->calc_context.x += p->x_stride;
-    y += p->y_stride;
+    p->calc_context.offset_x += p->x_stride;
+    p->calc_context.offset_y += p->y_stride;
+  }
+  return RT_FUNCTION_ERROR_NOERROR;
+}
+
+rt_function_error_t
+exec_pooling_generic(rt_function_t *f, pooling_context_t *context,
+                                 pooling_private_t *p,
+                                 exec_pooling_func_t exec) {
+  const int hx = p->input_shape.data[p->input_n_kernel_size_diff + 0];
+  const int wx = p->calc_context.wx;
+  const int hy = p->output_shape.data[p->input_n_kernel_size_diff + 0];
+  const int wy = p->output_shape.data[p->input_n_kernel_size_diff + 1];
+  const int hkernel = context->kernel.data[0];
+  const int wkernel = context->kernel.data[1];
+  const int hstride = context->stride.data[0];
+  const int wstride = context->stride.data[1];
+  const int hpad = context->pad.data[0];
+  const int wpad = context->pad.data[1];
+  const int n_map = calc_shape_size(f->inputs[0]->shape) / p->x_stride;
+  int n;
+  for (n = 0; n < n_map; n++) {
+    int iy;
+    for (iy = 0; iy < hy; iy++) {
+      int jy;
+      for (jy = 0; jy < wy; jy++) {
+        int hstart = iy * hstride - hpad;
+        int wstart = jy * wstride - wpad;
+        int hend = fminf(hstart + hkernel, hx + hpad);
+        int wend = fminf(wstart + wkernel, wx + wpad);
+        p->calc_context.pool_size = (hend - hstart) * (wend - wstart);
+        p->calc_context.hstart = fmaxf(hstart, 0);
+        p->calc_context.wstart = fmaxf(wstart, 0);
+        p->calc_context.hend = fminf(hend, hx);
+        p->calc_context.wend = fminf(wend, wx);
+        int k = iy * wy + jy;
+        float val = exec(p->calc_context);
+        p->calc_context.set_y(p->calc_context.y, k + p->calc_context.offset_y, val);
+      }
+    }
+    p->calc_context.offset_x += p->x_stride;
+    p->calc_context.offset_y += p->y_stride;
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
 float calc_max(pooling_calc_context_t calc) {
   int l = calc.hstart * calc.wx + calc.wstart;
-  float max_val = calc.x[l];
   int ix;
+  float *x = (float *)(calc.x->data);
+  float max_val = *(x + l + calc.offset_x);
   for (ix = calc.hstart; ix < calc.hend; ix++) {
     int jx;
     for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
-      float val = calc.x[jx];
+      float val = *(x + jx + calc.offset_x);
+      if (max_val < val) {
+        max_val = val;
+      }
+    }
+  }
+  return max_val;
+}
+
+float calc_max_generic(pooling_calc_context_t calc) {
+  int l = calc.hstart * calc.wx + calc.wstart;
+  int ix;
+  float max_val = calc.get_x(calc.x, l + calc.offset_x);
+  for (ix = calc.hstart; ix < calc.hend; ix++) {
+    int jx;
+    for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
+      float val = calc.get_x(calc.x, jx + calc.offset_x);
       if (max_val < val) {
         max_val = val;
       }
@@ -160,16 +223,47 @@ float calc_max(pooling_calc_context_t calc) {
 float calc_sum(pooling_calc_context_t calc) {
   float sum_val = 0.0f;
   int ix;
+  float *x = (float *)(calc.x->data);
   for (ix = calc.hstart; ix < calc.hend; ix++) {
     int jx;
     for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
-      sum_val += calc.x[jx];
+      sum_val += *(x + jx + calc.offset_x);
+    }
+  }
+  return sum_val;
+}
+
+float calc_sum_generic(pooling_calc_context_t calc) {
+  float sum_val = 0.0f;
+  int ix;
+  for (ix = calc.hstart; ix < calc.hend; ix++) {
+    int jx;
+    for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
+      sum_val += calc.get_x(calc.x, jx + calc.offset_x);
     }
   }
   return sum_val;
 }
 
 float calc_average(pooling_calc_context_t calc) {
+  float val = 0.0f;
+  float average_val = 0.0f;
+  float *x = (float *)(calc.x->data);
+  if (!calc.including_pad) {
+    calc.pool_size = (calc.hend - calc.hstart) * (calc.wend - calc.wstart);
+  }
+  int ix;
+  for (ix = calc.hstart; ix < calc.hend; ix++) {
+    int jx;
+    for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
+      val += *(x + jx + calc.offset_x);
+    }
+  }
+  average_val = val / calc.pool_size;
+  return average_val;
+}
+
+float calc_average_generic(pooling_calc_context_t calc) {
   float val = 0.0f;
   float average_val = 0.0f;
   if (!calc.including_pad) {
@@ -179,7 +273,7 @@ float calc_average(pooling_calc_context_t calc) {
   for (ix = calc.hstart; ix < calc.hend; ix++) {
     int jx;
     for (jx = ix * calc.wx + calc.wstart; jx < ix * calc.wx + calc.wend; jx++) {
-      val += calc.x[jx];
+      val += calc.get_x(calc.x, jx + calc.offset_x);
     }
   }
   average_val = val / calc.pool_size;
