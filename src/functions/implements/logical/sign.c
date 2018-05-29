820a32aca20b4afb5bb71e6b37171f9fa021799c
Liangliang.Zhou <Liangliang.Zhou@sony.com>
2018-05-29 13:56:14 +0800

    Implement logical generic func

--- src/functions/implements/logical/sign.c
+++ src/functions/implements/logical/sign.c
@@ -13,10 +13,19 @@
 // limitations under the License.
 
 #include "../../utilities/shape.h"
+#include "../../utilities/accessor.h"
 #include <nnablart/functions.h>
+
+rt_function_error_t exec_sign_generic(rt_function_t *f);
 
 // Sign
 rt_function_error_t allocate_sign_local_context(rt_function_t *f) {
+  if (f->inputs[0]->type == NN_DATA_TYPE_FLOAT &&
+      f->outputs[0]->type == NN_DATA_TYPE_FLOAT) {
+    f->exec_func = exec_sign;
+  } else {
+    f->exec_func = exec_sign_generic;
+  }
   return RT_FUNCTION_ERROR_NOERROR;
 }
 
@@ -33,6 +42,23 @@ rt_function_error_t exec_sign(rt_function_t *f) {
   int i;
   for (i = 0; i < output_size; ++i) {
     y[i] = (x[i] > 0) ? 1 : ((x[i] < 0) ? -1 : c->alpha);
+  }
+  return RT_FUNCTION_ERROR_NOERROR;
+}
+
+rt_function_error_t exec_sign_generic(rt_function_t *f) {
+  sign_local_context_t *c = (sign_local_context_t *)(f->local_context);
+  rt_variable_t *input = f->inputs[0];
+  rt_variable_getter get_input = select_getter(input);
+  rt_variable_t *output = f->outputs[0];
+  rt_variable_setter set_output = select_setter(output);
+
+  const int output_size = calc_shape_size(f->inputs[0]->shape);
+  int i;
+  for (i = 0; i < output_size; ++i) {
+    float x = get_input(input, i);
+    float y = (x > 0) ? 1 : ((x < 0) ? -1 : c->alpha);
+    set_output(output, i, y);
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
