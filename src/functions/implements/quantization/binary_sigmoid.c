42983a7bc8cf5fc73dec97517c7efb688d4416f8
Liangliang.Zhou <Liangliang.Zhou@sony.com>
2018-05-31 16:25:14 +0800

    Impelment binary_sigmoid and binary_tanh generic funcs

--- src/functions/implements/quantization/binary_sigmoid.c
+++ src/functions/implements/quantization/binary_sigmoid.c
@@ -13,15 +13,20 @@
 // limitations under the License.
 
 #include "../../utilities/shape.h"
+#include "../../utilities/accessor.h"
 #include <math.h>
 #include <nnablart/functions.h>
 
 typedef struct {
-  float *input;
+  rt_variable_t *input;
+  rt_variable_getter get_input;
   int input_size;
-  float *output;
+  rt_variable_t *output;
+  rt_variable_setter set_output;
   int output_size;
 } binary_sigmoid_private_context_t;
+
+rt_function_error_t exec_binary_sigmoid_generic(rt_function_t *f);
 
 // BinarySigmoid
 rt_function_error_t allocate_binary_sigmoid_local_context(rt_function_t *f) {
@@ -37,12 +42,20 @@ rt_function_error_t allocate_binary_sigmoid_local_context(rt_function_t *f) {
     return RT_FUNCTION_ERROR_MALLOC;
   }
   f->local_context = p;
-  p->input = f->inputs[0]->data;
+  p->input = f->inputs[0];
+  p->get_input = select_getter(p->input);
   p->input_size = calc_shape_size(f->inputs[0]->shape);
-  p->output = f->outputs[0]->data;
+  p->output = f->outputs[0];
+  p->set_output = select_setter(p->output);
   p->output_size = calc_shape_size(f->outputs[0]->shape);
   if (p->input_size != p->output_size) {
     return RT_FUNCTION_ERROR_INVALID_SHAPE;
+  }
+  if (p->input->type == NN_DATA_TYPE_FLOAT &&
+      p->output->type == NN_DATA_TYPE_FLOAT) {
+    f->exec_func = exec_binary_sigmoid;
+  } else {
+    f->exec_func = exec_binary_sigmoid_generic;
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
@@ -54,16 +67,35 @@ rt_function_error_t free_binary_sigmoid_local_context(rt_function_t *f) {
 rt_function_error_t exec_binary_sigmoid(rt_function_t *f) {
   binary_sigmoid_private_context_t *p =
       (binary_sigmoid_private_context_t *)(f->local_context);
+  float *input = (float *)(p->input->data);
+  float *output = (float *)(p->output->data);
   int i; // Iterator
   for (i = 0; i < p->output_size; i++) {
-    float x = (p->input[i] + 1) / 2;
+    float x = (input[i] + 1) / 2;
     if (x < 0) {
       x = 0;
     }
     if (x > 1) {
       x = 1;
     }
-    p->output[i] = roundf(x);
+    output[i] = roundf(x);
+  }
+  return RT_FUNCTION_ERROR_NOERROR;
+}
+
+rt_function_error_t exec_binary_sigmoid_generic(rt_function_t *f) {
+  binary_sigmoid_private_context_t *p =
+		(binary_sigmoid_private_context_t *)(f->local_context);
+  int i; // Iterator
+  for (i = 0; i < p->output_size; i++) {
+    float x = (p->get_input(p->input, i) + 1) / 2;
+    if (x < 0) {
+      x = 0;
+    }
+    if (x > 1) {
+      x = 1;
+    }
+    p->set_output(p->output, i, roundf(x));
   }
   return RT_FUNCTION_ERROR_NOERROR;
 }
