b2b46e8016be18f804ad1863079eb0a1c9a2f33a
Woody.Li <Woody.Li@sony.com>
2018-06-08 22:32:19 +0800

    using coeff instead of fp_pos

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -116,6 +116,7 @@ rt_return_value_t rt_initialize_context(rt_context_pointer context,
     c->variables[i].shape = create_rt_list_from_nn_list(n, var->shape);
     c->variables[i].type = var->type;
     c->variables[i].fp_pos = var->fp_pos;
+    c->variables[i].coefficient = var->coefficient;
     if (var->data_index < 0) {
       int index = (-1 * var->data_index) - 1;
       if (index >= c->num_of_buffers) {
