57bc534346a95d7f3d767e08bf59d1f169befe6d
Yuchi.Wen <Yuchi.Wen@sony.com>
2020-08-27 13:13:17 +0800

    Fix dependency error of build target.

--- build-tools/make/build-with-docker.mk
+++ build-tools/make/build-with-docker.mk
@@ -126,7 +126,7 @@ bwd-nnabla-c-runtime-update-function-info: nnabla-c-runtime-docker_image_test
 ########################################################################################################################
 # Check api_level of NNabla
 .PHONY: bwd-check-api_level
-bwd-check-api_level: nnabla-c-runtime-docker_image_build
+bwd-check-api_level: nnabla-c-runtime-docker_image_test
 	cd $(NNABLA_C_RUNTIME_DIRECTORY) \
 	&& docker run $(NNABLA_C_RUNTIME_DOCKER_RUN_OPTS) \
 		$(NNABLA_C_RUNTIME_DOCKER_IMAGE_TEST) make check-api_level
