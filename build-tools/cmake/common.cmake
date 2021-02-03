4384774d20ffbcb3a66f83b30dc2a6818dbed62b
VU Khac Tri <khactri.vu@sony.com>
2021-02-03 11:45:36 +0100

    Revert "fix build tool cmake"
    This reverts commit 7cbc5cc9ce7019ba4fa0ba79255ee31aa2a74405.

--- build-tools/cmake/common.cmake
+++ build-tools/cmake/common.cmake
@@ -22,6 +22,7 @@ set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
 # Compiler Settings.
 #-------------------------------------------------------------------------------
 
+set(TOOLCHAIN_PREFIX $ENV{TOOLCHAIN_PREFIX})
 set(CMAKE_TOOLCHAIN_FILE $ENV{CMAKE_TOOLCHAIN_FILE})
 
 if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
@@ -31,5 +32,6 @@ if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
   elseif(MSVC)
   endif()
 else()
+  message(INFO "==============" ${TOOLCHAIN_PREFIX})
   include(${CMAKE_TOOLCHAIN_FILE})
 endif()
